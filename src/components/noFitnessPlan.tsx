52fb76cd26cd087b34b17689e9c078df24613df5
farahsamad <farah_samad@outlook.com>
2025-06-13 18:54:04 +0300

    update files

--- src/components/noFitnessPlan.tsx
+++ src/components/noFitnessPlan.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { ArrowRightIcon } from "lucide-react";
-import CornerElements from "./cornerElement";
 
 const NoFitnessPlan = () => {
   return (
     <div className="relative backdrop-blur-sm border border-border rounded-lg p-10 text-center">
-      <CornerElements />
 
       <h2 className="text-2xl font-bold mb-4 font-mono">
         <span className="text-primary">No</span> fitness plans yet
